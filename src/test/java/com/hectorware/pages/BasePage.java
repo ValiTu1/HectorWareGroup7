8e1c325d33e7ef5e151f9ce1fba6a4f1835be4b3
Vali Tugui <vali.tuguii@gmail.com>
2022-03-01 20:23:20 +0000

    Initial commit

---
+++ src/test/java/com/hectorware/pages/BasePage.java
@@ -0,0 +1,25 @@
+package com.hectorware.pages;
+
+import com.hectorware.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    protected BasePage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(css = ".logo.logo-icon")
+    public WebElement logo;
+
+    @FindBy(className = "creatable")
+    public WebElement loader;
+
+    public void navigateMenuTab(String tabName){
+        Driver.get().findElement(By.xpath("(//a[@aria-label='"+tabName+"'])[1]")).click();
+    }
+
+}
---
+++ src/test/java/com/hectorware/step_definitions/Hooks.java
@@ -0,0 +1,23 @@
+package com.hectorware.step_definitions;
+
+import com.hectorware.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown(){
+
+        // Driver.closeDriver();
+    }
+
+}
