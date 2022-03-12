11332f4eeda58ba7b752729b394aaf3a1e5bf1f1
mustuner1972 <mustafaustuner1972@gmail.com>
2022-03-12 20:46:46 +0000

    Upload file functionality

--- src/test/java/com/hectorware/step_definitions/uploadFileDefs.java
+++ src/test/java/com/hectorware/step_definitions/uploadFileDefs.java
@@ -1,23 +1,64 @@
 package com.hectorware.step_definitions;
 
-import com.hectorware.pages.uploadFile;
+import com.hectorware.pages.UpLoadFile;
+import com.hectorware.utilities.Driver;
+import com.hectorware.utilities.Utils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class uploadFileDefs {
-    uploadFile upload = new uploadFile();
+    UpLoadFile upload = new UpLoadFile();
 
     @Then("user clicks addButton")
     public void userClicksAddButton() {
         upload.addBtn();
 
     }
-
-
     @And("user chooses {string} option")
     public void userChoosesOption(String uploadOption) {
         upload.uploadOption(uploadOption);
+    }
+
+    @Then("user selects uploaded file from directory")
+    public void userSelectsUploadedFileFromDirectory() throws AWTException {
+
+        upload.uploadFile();
+        Utils.wait(5);
+        Assert.assertTrue("file is not uploaded", upload.uploadedFile.isDisplayed());
+        Utils.wait(7);
+        upload.uploadedFileSelect.click();
+        upload.ActionsDropdown.click();
+        upload.actionsDelete.click();
+
+        //Assert.assertTrue("file is not deleted", upload.uploadedFile.isDisplayed());
+
+    }
+
+    @Then("user creates New folder in the page")
+    public void userCreatesNewFolderInThePage() {
+
+        Utils.wait(4);
+        upload.newFolderName.sendKeys("new Folder new");
+        Utils.wait(3);
+        upload.newFolderArrow.click();
+        Utils.wait(4);
+
+        Assert.assertTrue("uploaded file is not seen", upload.uploadedFolder.isDisplayed());
+
+        // to delete the uploaded folder
+        Driver.get().findElement(By.xpath("//tr[@data-file='new Folder new']/td")).click();
+        upload.ActionsDropdown.click();
+        upload.actionsDelete.click();
+
     }
 }
